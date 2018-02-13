8c68eb80028b9ddbc1a3fd1f4461164ff74117ae
David Akre <dakre21@gmail.com>
2018-02-13 10:53:52 -0700

    Creating SS for bfs

--- hw01_dakre/README.txt
+++ hw01_dakre/README.txt
@@ -1,2 +1,2 @@
-Programming: C++
+Programming: C++11
 
--- hw01_dakre/src/hw01_dakre_bfs.h
+++ hw01_dakre/src/hw01_dakre_bfs.h
@@ -6,12 +6,48 @@
 #include <cstdlib>
 #include <stdio.h>
 #include <queue>
+#include <list>
 
 using namespace std;
 
+class SearchSpace {
+public:
+    SearchSpace(int num_vertices) {
+        this->_num_vertices = num_vertices;
+        this->_adj_pair = new list<pair<int, int> >[num_vertices];
+    };
+
+    ~SearchSpace() { 
+        // Do nothing
+    };
+
+    void addEdge(int index, pair<int, int> state) {
+        this->_adj_pair[index].push_back(state);
+    };
+
+    void bfs(int index) {
+        // Perform BFS at index
+    };
+     
+
+private:
+    int _num_vertices;
+    list<pair<int, int> >* _adj_pair; 
+};
+
 void strategyB(string file_name, pair<int, int> curr_state, queue<pair<int, int> >, 
     int goal_jug_a, int goal_jug_b, int cap_jug_a, int cap_jug_b) {
-    // Forward declarations
+    // Create state space
+    int count = 0;
+    int size = (cap_jug_a + 1) * (cap_jug_b + 1);
+    SearchSpace ss(size); 
+
+    for (int i = 0; i < (cap_jug_a + 1); i++) {
+        for (int j = 0; j < (cap_jug_b + 1); j++) {
+            ss.addEdge(count, {i, j}); 
+            count++;
+        }
+    }
 
     // Append to the output file
     ofstream out_file;
