f21fc2059a1b8610d317002031e93302ffc5c770
David Akre <dakre21@gmail.com>
2018-02-13 14:08:25 -0700

    Working on bfs algo

--- hw01_dakre/src/hw01_dakre_bfs.h
+++ hw01_dakre/src/hw01_dakre_bfs.h
@@ -24,7 +24,6 @@ class SearchSpace {
 public:
     SearchSpace(int num_vertices, map<pair<int, int>, bool> states) {
         this->_num_vertices = num_vertices;
-        //this->_adj_pair = new list<pair<int, int> >[num_vertices];
         this->_states = states; 
     };
 
@@ -32,56 +31,136 @@ public:
         // Do nothing
     };
 
-    /*void addEdge(int index, pair<int, int> state) {
-        this->_adj_pair[index].push_back(state);
-    };*/
-
-    void bfs(pair<int, int> state) {
+    void bfs(pair<int, int> state, int cap_jug_a, int cap_jug_b, int goal_jug_a, int goal_jug_b, string file_name) {
         // Forward declarations
+        int fill_amount_a         = 0;
+        int fill_amount_b         = 0;
+        int fill                  = 0;
         pair<int, int> curr_state = state;
         queue<pair<int, int> > path;
-        map<pair<int, int> bool> pot_states;
-        map<pair<int, int>, bool>::iterator it;
+        map<pair<int, int>, int> pot_states;
+        map<pair<int, int>, bool>::iterator sub_it;
+        map<pair<int, int>, int>::iterator it;
 
         // Mark index node as states
         this->_states[state] = 1;
         path.push(state);
+
+        // Append to the output file
+        ofstream out_file;
+        out_file.open(file_name, ofstream::out | ofstream::app);
+
+        cout << ">Strategy B" << endl;
+        cout << ">Starting out with a " << cap_jug_a << "-gal jug and a " << cap_jug_b << 
+            "-gal jug --- state:(" << curr_state.first << "," << curr_state.second << ")" << endl;
+        out_file << ">Strategy B\n";
+        out_file << ">Starting out with a " << cap_jug_a << "-gal jug and a " << cap_jug_b << 
+            "-gal jug --- state:(" << curr_state.first << "," << curr_state.second << ")\n";
 
         while (!path.empty()) {
             // Dequeue curr_state top vertex
             curr_state = path.front();
             path.pop();
 
+            for (it = pot_states.begin(); it != pot_states.end(); ++it) {
+                if (curr_state == it->first) {
+                    switch (it->second) {
+                        case EMPTYA :
+                            cout << ">Empty the " << cap_jug_a << "-gal jug";
+                            out_file << ">Empty the " << cap_jug_a << "-gal jug";
+                            cout << " --- state: (" << curr_state.first << "," << curr_state.second << ")" << endl;
+                            out_file << " --- state: (" << curr_state.first << "," << curr_state.second << ")\n";
+                            break;
+
+                        case EMPTYB :
+                            cout << ">Empty the " << cap_jug_b << "-gal jug";
+                            out_file << ">Empty the " << cap_jug_b << "-gal jug";
+                            cout << " --- state: (" << curr_state.first << "," << curr_state.second << ")" << endl;
+                            out_file << " --- state: (" << curr_state.first << "," << curr_state.second << ")\n";
+                            break;
+
+                        case FILLA :
+                            cout << ">Fill the " << cap_jug_a << "-gal jug";
+                            out_file << ">Fill the " << cap_jug_a << "-gal jug";
+                            cout << " --- state: (" << curr_state.first << "," << curr_state.second << ")" << endl;
+                            out_file << " --- state: (" << curr_state.first << "," << curr_state.second << ")\n";
+                            break;
+       
+                        case FILLB :
+                            cout << ">Fill the " << cap_jug_b << "-gal jug";
+                            out_file << ">Fill the " << cap_jug_b << "-gal jug";
+                            cout << " --- state: (" << curr_state.first << "," << curr_state.second << ")" << endl;
+                            out_file << " --- state: (" << curr_state.first << "," << curr_state.second << ")\n";
+                            break;
+
+                        case POURAB :
+                            cout << ">Pour water from the " << cap_jug_a << "-gal jug into the " 
+                                << cap_jug_b << "-gal jug";
+                            out_file << ">Pour water from the " << cap_jug_a << "-gal jug into the " 
+                                << cap_jug_b << "-gal jug";
+                            cout << " --- state: (" << curr_state.first << "," << curr_state.second << ")" << endl;
+                            out_file << " --- state: (" << curr_state.first << "," << curr_state.second << ")\n";
+                            break;
+
+                        case POURBA :
+                            cout << ">Pour water from the " << cap_jug_b << "-gal jug into the " 
+                                << cap_jug_a << "-gal jug";
+                            out_file << ">Pour water from the " << cap_jug_b << "-gal jug into the " 
+                                << cap_jug_a << "-gal jug";
+                            cout << " --- state: (" << curr_state.first << "," << curr_state.second << ")" << endl;
+                            out_file << " --- state: (" << curr_state.first << "," << curr_state.second << ")\n";
+                            break;
+         
+                        default:
+                            break;
+                    }
+                    break;
+                }
+            }
+            
+            // Check if goal state is reached
+            if ((curr_state.first == goal_jug_a && curr_state.second == goal_jug_b) ||
+                (goal_jug_a == -1 && curr_state.first <= cap_jug_a && 
+                curr_state.second == goal_jug_b) ||
+                (goal_jug_b == -1 && curr_state.second <= cap_jug_b &&
+                curr_state.first == goal_jug_a) ||
+                (goal_jug_a == -1 && goal_jug_b == -1)) {
+                break;
+            }
+
+            // Reset potential states
+            pot_states.clear();
+
             // Analyze all possible moves as new potential states
             for (int op = 0; op < NOPERATORS; op++) {
-                switch (rnum) {
+                switch (op) {
                     case EMPTYA :
-                        if (curr_state.first == 0) {
-                            bad_move = true;
+                        if (curr_state.first > 0) {
+                            pot_states.insert(pair<pair<int, int>, int>({0, curr_state.second}, EMPTYA));  
                         }
                         break;
                         
                     case EMPTYB :
-                        if (curr_state.second == 0) {
-                            bad_move = true;
+                        if (curr_state.second > 0) {
+                            pot_states.insert(pair<pair<int, int>, int>({curr_state.first, 0}, EMPTYB));  
                         }
                         break;
          
                     case FILLA :
-                        if (curr_state.first == cap_jug_a) {
-                            bad_move = true;
+                        if (curr_state.first < cap_jug_a) {
+                            pot_states.insert(pair<pair<int, int>, int>({cap_jug_a, curr_state.second}, FILLA));  
                         }
                         break;
          
                     case FILLB :
-                        if (curr_state.second == cap_jug_b) {
-                            bad_move = true;
+                        if (curr_state.second < cap_jug_b) {
+                            pot_states.insert(pair<pair<int, int>, int>({curr_state.first, cap_jug_b}, FILLB));  
                         }
                         break;
          
                     case POURAB :
                         if (((cap_jug_b - curr_state.second) == 0) || (curr_state.first == 0)) {
-                            bad_move = true;
+                            break;
                         }
 
                         fill_amount_b = cap_jug_b - curr_state.second;
@@ -92,11 +171,13 @@ public:
                             fill = fill_amount_b;
                         }
 
+                        pot_states.insert(pair<pair<int, int>, int>({curr_state.first - fill, curr_state.second + fill}, POURAB));
+
                         break;
          
                     case POURBA :
                         if (((cap_jug_a - curr_state.first) == 0) || (curr_state.second == 0)) {
-                            bad_move = true;
+                            break;
                         }
 
                         fill_amount_a = cap_jug_a - curr_state.first;
@@ -107,6 +188,8 @@ public:
                             fill = fill_amount_a;
                         }
 
+                        pot_states.insert(pair<pair<int, int>, int>({curr_state.first + fill, curr_state.second - fill}, POURAB));
+
                         break;
 
                     default:
@@ -114,14 +197,20 @@ public:
                 }
             }
 
-            for (it = this->_states.begin(); it != this->_states.end(); ++it) {
-                if (it->second == false) {
-                    it->second = true;
-                    path.push(it->first);
+            for (it = pot_states.begin(); it != pot_states.end(); ++it) {
+                for (sub_it = this->_states.begin(); sub_it != this->_states.end(); ++sub_it) {
+                    if (it->first == sub_it->first) {
+                        if (sub_it->second == false) {
+                            sub_it->second = true;
+                            path.push(sub_it->first);
+                        }
+                        break;
+                    }
                 }
             }            
         }
-        
+
+        out_file.close();
     };
      
 
@@ -144,28 +233,7 @@ void strategyB(string file_name, pair<int, int> curr_state, int goal_jug_a,
         }
     }
 
-    // Create StateSpace object
+    // Create StateSpace object and invoke bfs algorithm
     SearchSpace ss(size, states); 
-
-    // TODO: Refactor this to setup edges properly per water jug problem
-    /*for (int i = 0; i < (cap_jug_a + 1); i++) {
-        for (int j = 0; j < (cap_jug_b + 1); j++) {
-            ss.addEdge(count, make_pair(i, j)); 
-            count++;
-        }
-    }*/
-
-    // Append to the output file
-    ofstream out_file;
-    out_file.open(file_name, ofstream::out | ofstream::app);
-
-    cout << ">Strategy B" << endl;
-    cout << ">Starting out with a " << cap_jug_a << "-gal jug and a " << cap_jug_b << 
-        "-gal jug --- state:(" << curr_state.first << "," << curr_state.second << ")" << endl;
-    out_file << ">Strategy B\n";
-    out_file << ">Starting out with a " << cap_jug_a << "-gal jug and a " << cap_jug_b << 
-        "-gal jug --- state:(" << curr_state.first << "," << curr_state.second << ")\n";
-    
-    ss.bfs(curr_state);
-    out_file.close();
+    ss.bfs(curr_state, cap_jug_a, cap_jug_b, goal_jug_a, goal_jug_b, file_name);
 } 
